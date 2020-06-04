8ef7c6e8e2047582299dfba88a211ee45247b1de
Zeeshan Azmat <zeeshanazmat56@gmail.com>
2020-06-04 19:11:51 +0500

    changes

--- App.js
+++ App.js
@@ -6,7 +6,7 @@ const App = () => {
     <SafeAreaView>
       <View styles={styles.container}>
         <Text>Hello, World. 123</Text>
-        <Text>Hiya</Text>
+        <Text>Hiya!</Text>
       </View>
     </SafeAreaView>
   );
