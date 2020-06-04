eca2b9826bd35831dd22028d4735117b6795e36b
Zeeshan Azmat <zeeshanazmat56@gmail.com>
2020-06-04 19:04:56 +0500

    first commit

--- App.js
+++ App.js
@@ -1,14 +1,21 @@
 import React from 'react';
-import { View, Text, SafeAreaView } from 'react-native';
+import { View, Text, SafeAreaView, StyleSheet } from 'react-native';
 
 const App = () => {
   return (
     <SafeAreaView>
-      <View>
-        <Text>Hello, World!</Text>
+      <View styles={styles.container}>
+        <Text>Hello, World. 123</Text>
+        <Text>Hiya</Text>
       </View>
     </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    margin: 10,
+  },
+});
 
 export default App;
