b40f61bf8a65cb54ea696d7bd9e0f86e62bbc23f
Zeeshan Azmat <zeeshanazmat56@gmail.com>
2020-06-04 17:52:27 +0500

    initial commit

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text, SafeAreaView } from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>Hello, World!</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default App;
