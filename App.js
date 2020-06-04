60b1eb1292b76e03ff1bdfeb6308db2e32727756
Zeeshan Azmat <zeeshanazmat56@gmail.com>
2020-06-05 01:24:00 +0500

    styles

--- App.js
+++ App.js
@@ -3,18 +3,59 @@ import { View, Text, SafeAreaView, StyleSheet } from 'react-native';
 
 const App = () => {
   return (
-    <SafeAreaView>
-      <View styles={styles.container}>
-        <Text>Hello, World. 123</Text>
-        <Text>Hiya! Tried new thing Hurrayyyy</Text>
+    <SafeAreaView style={[styles.safeAreaView]}>
+      <View>
+        <Text style={styles.bold}>
+          Here are some boxes of different colors!
+        </Text>
+      </View>
+
+      <View style={[styles.container, styles.cyan]}>
+        <Text>Cyan #2aa198</Text>
+      </View>
+
+      <View style={[styles.container, styles.blue]}>
+        <Text>Blue #268bd2</Text>
+      </View>
+
+      <View style={[styles.container, styles.magenta]}>
+        <Text>Magenta #d33682</Text>
+      </View>
+
+      <View style={[styles.container, styles.orange]}>
+        <Text>Orange #cb4b16</Text>
       </View>
     </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
+  cyan: {
+    backgroundColor: '#2aa198',
+  },
+  blue: {
+    backgroundColor: '#268bd2',
+  },
+  magenta: {
+    backgroundColor: '#d33682',
+  },
+  orange: {
+    backgroundColor: '#cb4b16',
+  },
+  safeAreaView: {
+    marginVertical: 20,
+    marginHorizontal: 20,
+    flex: 1,
+  },
+  bold: {
+    fontSize: 15,
+    fontWeight: 'bold',
+  },
   container: {
-    margin: 10,
+    marginVertical: 5,
+    paddingVertical: 10,
+    alignItems: 'center',
+    paddingHorizontal: 10,
   },
 });
 
